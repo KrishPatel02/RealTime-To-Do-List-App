f837641f744349040695818ae3f35753f641d47b
Krish Patel <krishpatel200508@gmail.com>
2024-06-29 16:06:50 +0530

    Update Task List Box

--- src/Components/TaskListBox.jsx
+++ src/Components/TaskListBox.jsx
@@ -6,38 +6,40 @@ import { List } from "@mui/material";
 import "../ComponentsCSS/TaskListBox.css";
 
 const TaskListBox = ({
-    toDoTasks,
-    completedTasks,
+    tasks,
     onDelete,
     onMarkComplete,
     onEdit,
     currentCategory,
 }) => {
+    const filteredTasks = tasks.filter((task) =>
+        currentCategory === "toDo" ? !task.completed : task.completed
+    );
+
     const renderTasks = (tasks) =>
         tasks.map((task, index) => (
             <TaskItem
-                key={task.id}  // Ensure each task has a unique identifier
+                key={task.id}
                 task={task}
-                index={index}  // Pass the index to maintain uniqueness
-                onDelete={onDelete}  // Pass deleteTask function
+                index={index}
+                onDelete={onDelete}
                 onMarkComplete={onMarkComplete}
                 onEdit={onEdit}
                 currentCategory={currentCategory}
             />
         ));
 
-
     return (
         <>
             {currentCategory === "toDo" ? (
                 <List className="taskListBox">
                     <h2>To Do Tasks</h2>
-                    {renderTasks(toDoTasks)}
+                    {renderTasks(filteredTasks)}
                 </List>
             ) : (
                 <List className="taskListBox">
                     <h2>Completed Tasks</h2>
-                    {renderTasks(completedTasks)}
+                    {renderTasks(filteredTasks)}
                 </List>
             )}
         </>
